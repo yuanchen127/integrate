21e1a6f9b22ef1cb94a4d6ef0e486fd734752ed0
yuanchen127 <1091355579@qq.com>
2024-02-27 19:04:08 +0800

    task: 依赖版本整理

--- eureka-server/src/main/java/org/ike/neo4j/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/ike/neo4j/EurekaServerApplication.java
@@ -12,5 +12,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+//        org.springframework.boot.context.properties.ConfigurationBeanFactoryMetadata
     }
 }
